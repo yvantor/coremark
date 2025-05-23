20891ea29e88e879a7a9c954904bf3120e2a89a9
Yvan Tortorella <ytortorella@lagrev5.ee.ethz.ch>
2025-05-23 23:54:36 +0200

    Delete CLOCKS_PER_SEC hardcoded define.

--- cheshire/core_portme.c
+++ cheshire/core_portme.c
@@ -56,7 +56,6 @@ barebones_clock()
    does not occur. If there are issues with the return value overflowing,
    increase this value.
         */
-#define CLOCKS_PER_SEC             100
 #define GETMYTIME(_t)              (*_t = barebones_clock())
 #define MYTIMEDIFF(fin, ini)       ((fin) - (ini))
 #define TIMER_RES_DIVIDER          1
